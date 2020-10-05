b221e0b194b26cc49035df06ea34e95bc3dfe08f
skb3k <eifnogso.ezb8@gmail.com>
2020-10-05 21:52:47 +0900

    changed webpack.config.js to reproduce package bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,39 @@
+const path = require('path');
+
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'node'
-  };
+  mode: 'production',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  target: 'node',
+  externals: {
+    //require: 'require',
+  }
+};
+
+
+//const path = require('path');
+//
+//module.exports = {
+//  mode: 'production',
+//  // エントリーポイントの設定
+//  entry: {
+//    server: './server.js',
+//    game: './static/game.js',
+//  },
+//  // ビルド後、'./dist/my-bundle.js'というbundleファイルを生成する
+//  output: {
+//    path: path.resolve(__dirname, 'dist'),
+//    filename: '[name].bundle.js'
+//  },
+//  target: 'node',
+//  externals: {
+//    //require: 'require',
+//    //bufferutil: 'bufferutil',
+//    //utfvalidate: 'utfvalidate',
+//  },
+//};
