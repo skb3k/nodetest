39312cd7a9aa237bd8e926864ac6903754f3d597
skb3k <eifnogso.ezb8@gmail.com>
2020-10-05 22:29:23 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -14,26 +14,3 @@ module.exports = {
     //require: 'require',
   }
 };
-
-
-//const path = require('path');
-//
-//module.exports = {
-//  mode: 'production',
-//  // エントリーポイントの設定
-//  entry: {
-//    server: './server.js',
-//    game: './static/game.js',
-//  },
-//  // ビルド後、'./dist/my-bundle.js'というbundleファイルを生成する
-//  output: {
-//    path: path.resolve(__dirname, 'dist'),
-//    filename: '[name].bundle.js'
-//  },
-//  target: 'node',
-//  externals: {
-//    //require: 'require',
-//    //bufferutil: 'bufferutil',
-//    //utfvalidate: 'utfvalidate',
-//  },
-//};
